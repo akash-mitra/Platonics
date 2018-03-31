22cd719a739a0922a30eef16d017a2e669059478
Akash Mitra <akashmitra@gmail.com>
2018-04-01 02:32:53 +0800

    Cleaned up BaseController, added comments

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -5,26 +5,18 @@ namespace App\Http\Controllers;
 use App\Configuration;
 use Illuminate\Support\Facades\View;
 
+/**
+ * Use BaseController when you need to inject
+ * additional meta data information to page
+ */
 class BaseController extends Controller
 {
     protected $meta; 
 
     public function __construct ()
-    {
-        
+    {   
         $this->meta = unserialize (Configuration::where('key', 'blog')->first()->value); 
 
         View::share('meta', $this->meta);
     }
-
-    // $this->meta = ["bg-color-primary" => "#FFFFFF",
-    //     "enable-terms" => "1",
-    //     "enable-privacy" => "1",
-    //     "layout" => "right",
-    //     "blogName" => "BDO Birpara Madarihat",
-    //     "blogDesc" => "1",
-    //     "enable-about" => "0",
-    //     "left-modules" => ["about", "popular"]];
-
-    
 }
