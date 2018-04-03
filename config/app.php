b577f42ece9125fcbe0e2cea54d887934a8706d4
Akash Mitra <akashmitra@gmail.com>
2018-04-03 22:19:42 +0800

    Manual config changes for 5.5 upgrade

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Blog'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -176,11 +176,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        // login service providers
-        Laravel\Socialite\SocialiteServiceProvider::class,
-
-        // flash messaging service provider
-        Laracasts\Flash\FlashServiceProvider::class,
     ],
 
     /*
@@ -229,7 +224,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
